91f42ec89c0cc8888e14888666e75020a0ff44f6
Mathieu Heynen <contact.heynen@gmail.com>
2024-05-24 12:30:29 +0200

    challenge2 day29 create the controller, models, routes and views

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,35 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+    def destroy
+      @list.destroy
+      redirect_to lists_path, status: :see_other
+    end
+  end
+
+  #--PRIVATE--
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
