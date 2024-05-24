7bad4a3bfc4ff368a5c040ee4777a179ea087ffb
Mathieu Heynen <contact.heynen@gmail.com>
2024-05-24 17:06:29 +0200

    challenge2 day29 solved

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,4 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
   before_action :set_list, only: [:new, :create]
 
   def new
@@ -17,6 +16,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+
     @bookmark.destroy
     redirect_to list_path(@bookmark.list), status: :see_other
   end
@@ -25,10 +26,6 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
-  end
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
   end
 
   def set_list
